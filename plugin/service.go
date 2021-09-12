bb7ee672c23f381f81994545aebef9e0f4077b49
刘桂林 <liu_guilin@qq.com>
2021-09-12 17:31:22 +0800

    update

--- plugin/service.go
+++ plugin/service.go
@@ -1,26 +1,49 @@
-package plugin
+package models
 
-import "github.com/galenliu/gateway/pkg/rpc"
+import (
+	"github.com/galenliu/gateway/pkg/rpc"
+)
+
+type Plugin interface {
+	SendMsg(rpc.MessageType, map[string]interface{})
+}
+
+type Bus interface {
+	SubscribePropertyChanged(f func(property *Property))
+	UnsubscribePropertyChanged(f func(property *Property))
+}
 
 type Service struct {
 	ID     string
 	Name   string
-	plugin *Plugin
+	bus    Bus
+	plugin Plugin
 }
 
-func NewServiceProxy(plugin *Plugin, id string, name string) *Service {
+func NewService(plugin Plugin, bus Bus, id string, name string) *Service {
 	s := &Service{}
+	s.bus = bus
 	s.plugin = plugin
 	s.ID = id
 	s.Name = name
+	s.bus = bus
+	s.bus.SubscribePropertyChanged(s.handlePropertyChanged)
 	return s
 }
 
-func (s *Service) handlePropertyChanged() {
-
+func (s *Service) handlePropertyChanged(property *Property) {
+	data := make(map[string]interface{})
+	data["thingId"] = property.Device.GetID()
+	data["propertyName"] = property.Name
+	data["value"] = property.Value
+	s.sendMsg(rpc.MessageType_ServicePropertyChangedNotification, data)
 }
 
 func (s *Service) sendMsg(messageType rpc.MessageType, data map[string]interface{}) {
 	data["serviceId"] = s.ID
 	s.plugin.SendMsg(messageType, data)
+}
+
+func (s *Service) unload() {
+	s.bus
 }
