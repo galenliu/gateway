cbd52f79591fffe5d73cf6a33697611896b9c52f
galenliu <galenliu@live.con>
2021-12-26 12:43:26 +0800

    Changes

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"github.com/galenliu/gateway/cmd/gateway/cmd"
 	"os"
-	"os/signal"
-	"syscall"
 )
 
 var (
@@ -17,26 +15,6 @@ var (
 func init() {
 	flag.StringVar(&proFile, "profile", "", "Profile directory")
 	flag.BoolVar(&showVersion, "version", false, "version")
-}
-
-// TermFunc defines the function which is executed on termination.
-type TermFunc func(sig os.Signal)
-
-// OnTermination calls a function when the app receives an interrupt of kill signal.
-func OnTermination(fn TermFunc) {
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt)
-	signal.Notify(c, os.Kill)
-	signal.Notify(c, syscall.SIGTERM)
-
-	func() {
-		select {
-		case sig := <-c:
-			if fn != nil {
-				fn(sig)
-			}
-		}
-	}()
 }
 
 func main() {
